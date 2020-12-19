95c3679b87ec03bc91e9dc4846dce5f6e42c4e8e
Ajay Kumar <ajaykumar005097@gmail.com>
2020-12-20 01:03:07 +0530

    Update NearestSlotFetcherService.java
    LGTM

--- src/com/company/parkingLot/NearestSlotFetcherService.java
+++ src/com/company/parkingLot/NearestSlotFetcherService.java
@@ -4,6 +4,11 @@ public class NearestSlotFetcherService implements SlotFetcher {
     @Override
     public Slot getSlot(int EntryGate, Vehicle vehicle, ParkingLot parkingLot) {
         //TODO: implement algorithm to fetch the nearest slot to the entry point
+        
         return null;
+    }
+    
+    public boolean getAccess() {
+        return true;
     }
 }
